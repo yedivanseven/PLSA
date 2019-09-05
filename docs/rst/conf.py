a12d2c235d6585484966326f1bf3a47f9b1d607f
Georg Heimel <georg@muckisnspirit.com>
2019-09-05 15:25:31 +0200

    Changed path in shpinx conf.py to relative.

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/home/georg/Documents/PLSA'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
