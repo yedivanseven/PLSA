f296d1f61a0cb1a9b0ae48a9ed698fba2ddab57c
Georg Heimel <georg@muckisnspirit.com>
2019-09-06 13:33:03 +0200

    Bumped up to version 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='plsa',
-    version='0.1.1',
+    version='0.2.0',
     description='Probabilistic Latent Semantic Analysis',
     long_description=long_description,
     long_description_content_type='text/markdown',
